8dce12e09eeb71b1683316f8da43cec572178c54
TasfiqulTapu <66732331+TasfiqulTapu@users.noreply.github.com>
2025-10-05 19:18:30 +0600

    Added .gitignore check to verify no node_modules makes it to github

--- blank.js
+++ blank.js
@@ -3,18 +3,29 @@
 import { readdirSync, writeFileSync } from 'fs';
 import path from 'path';
 import data from "./data.js"
-const currentDirectory = process.cwd();
 
-console.log(data);
+const currentDirectory = process.cwd();
 const files = readdirSync(currentDirectory, "utf-8");
 
 if(files.includes("package.json"))
-    console.log("this directory contain a package.json.\nAre you sure you want to create a new package?");
+    console.log("😾 This directory contains a \x1b[;33mpackage.json\x1b[;0m.\nAre you sure you want to create a new package?");
 else{
     const packagePath = path.join(currentDirectory, "package.json");
-    // writeFileSync(packagePath, )
+    const pjContents = data["package.json"];
+    pjContents.name = path.basename(process.cwd());
+    console.log(`Creating package.json`);
+    console.log(pjContents);
+    writeFileSync(packagePath, JSON.stringify(pjContents, null, 4));
+    console.log("😸 package.json created!");
 }
-const contents = data["package.json"];
-contents.name = path.basename(process.cwd());
-console.log(`Creating package.json`);
-console.log(contents);
+if(files.includes(".gitignore")) 
+    console.log("😾 This directory contains a \x1b[;33m.gitignore\x1b[;0m.\nAre you sure you want to create a new package?");
+else{ 
+    const gitignorePath = path.join(currentDirectory, ".gitignore");
+    const giContents = data[".gitignore"];
+    console.log(`Creating .gitgnore`);
+    console.log(giContents);
+    writeFileSync(gitignorePath, giContents);
+    console.log("😸 .gitignore created!");
+
+}
