c2db57a77e3e7fac1ac11af18a6a36959729bf9f
TasfiqulTapu <66732331+TasfiqulTapu@users.noreply.github.com>
2025-10-05 16:13:30 +0600

    started creating package.json

--- blank.js
+++ blank.js
@@ -1,3 +1,20 @@
 #!/usr/bin/env node
 
-console.log("Hello World")
+import { readdirSync, writeFileSync } from 'fs';
+import path from 'path';
+import data from "./data.js"
+const currentDirectory = process.cwd();
+
+console.log(data);
+const files = readdirSync(currentDirectory, "utf-8");
+
+if(files.includes("package.json"))
+    console.log("this directory contain a package.json.\nAre you sure you want to create a new package?");
+else{
+    const packagePath = path.join(currentDirectory, "package.json");
+    // writeFileSync(packagePath, )
+}
+const contents = data["package.json"];
+contents.name = path.basename(process.cwd());
+console.log(`Creating package.json`);
+console.log(contents);
---
+++ data.js
@@ -0,0 +1,17 @@
+export default
+{
+  "package.json":
+      {
+          "name": "@tapu/blank",
+          "version": "1.0.0",
+          "description": "",
+          "type": "module",
+          "main": "index.js",
+          "scripts": {
+            "run": "node index.js"
+          },
+          "keywords": [],
+          "author": "",
+          "license": "MIT"
+      },
+}
